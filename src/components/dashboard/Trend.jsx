305886605572e53777212b7ed616474df62b5738
zhangchao <544262408@qq.com>
2018-03-07 10:30:10 +0800

    趋势页面的简单逻辑

--- src/components/dashboard/Trend.jsx
+++ src/components/dashboard/Trend.jsx
@@ -4,67 +4,91 @@ import { Row, Col, Card, Timeline, Icon,Tabs,Button,Menu, Dropdown,Radio,Table }
 import BreadcrumbCustom from '../BreadcrumbCustom';
 import css from './cus.css';
 import 'whatwg-fetch';
+import * as moment from 'moment';
 
 
 const ReactHighcharts = require('react-highcharts');
 
-
+function dataForDay(data) {
+  console.log(moment)
+  return  _.map(data,function(item) {
+    return {
+      x:moment(item.time).toDate().getTime(),
+      // x:item.time,
+      y:item.item.num!=undefined?item.item.num:0
+    }
+  })
+}
 
 const columns = [{
     title: '时间',
-    dataIndex: 'name',
-    key: 'name',
+    dataIndex: 'time',
+    key: 'time',
     render: text => <a href="#/app/dashboard/index">{text}</a>,
 }, {
     title: '指标',
     dataIndex: 'age',
     key: 'age',
-}, {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
-}, {
-    title: 'Action',
-    key: 'action',
-    render: (text, record) => (
-        <span>
-      <a>Action 一 {record.name}</a>
-      <span className="ant-divider" />
-      <a>Delete</a>
-      <span className="ant-divider" />
-      <a className="ant-dropdown-link">
-        More actions <Icon type="down" />
-      </a>
-    </span>
-    ),
 }];
+var keyvalue={
+  new_vistior_line:'新用用户数',
+  vistior_line:'访问人数',
+  page_line:'访问次数'
+}
 
-const data = [{
-    key: '1',
-    name: 's',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
-}, {
-    key: '2',
-    name: 'Jim Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
-}, {
-    key: '3',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park',
-}];
 class Trend extends React.Component {
   constructor(props) {
     super(props);
+    this.daytypeclick=this.daytypeclick.bind(this);
 
 
     this.state = {
+      linetoday:1,
+      linetype:'page_line'
     };
   }
   componentDidMount() {
+    this.getlinedata(1,'page_line');
 
+  }
+  menu = (
+    <Menu onClick={this.handleMenuClick.bind(this)}>
+      <Menu.Item key="new_vistior_line" >
+        新用用户数
+      </Menu.Item>
+      <Menu.Item key="vistior_line">
+        访问人数
+      </Menu.Item>
+      <Menu.Item key="page_line">
+        访问次数
+      </Menu.Item>
+    </Menu>
+  );
+  getlinedata(today,linetype) {
+    var _this=this;
+    this.setState({linetoday:today,linetype:linetype})
+    fetch('/showdata/9885790/'+linetype+'/'+today)
+      .then(function(response) {
+        return response.json()
+      }).then(function(jsonData) {
+         _this.setState({
+           listData:jsonData,
+           chart:{
+               name: keyvalue[linetype],
+               data: dataForDay(jsonData)
+           }
+
+         })
+      })
+  }
+  handleMenuClick(e){
+    console.log(e.key);
+    this.getlinedata(this.state.linetoday,e.key)
+
+  }
+  daytypeclick(e){
+   console.log(e.target.value)
+   this.getlinedata(e.target.value,this.state.linetype)
   }
   render() {
     var  config={
@@ -80,7 +104,7 @@ class Trend extends React.Component {
         }
     },
     xAxis: {
-        type:'category'
+        type:'datetime'
     },
 
     legend: {
@@ -96,13 +120,7 @@ class Trend extends React.Component {
             pointStart: 2010
         }
     },
-    series: [
-      {
-        name:'指标',
-        data:[111,222,33334,4444]
-      }
-
-    ],
+    series: [this.state.chart],
     responsive: {
         rules: [{
             condition: {
@@ -125,11 +143,18 @@ class Trend extends React.Component {
                 <div className="gutter-box">
                     <Card bordered={false}
                     title={ <Radio.Group onChange={this.daytypeclick} >
+                            <Radio.Button value="0">今天</Radio.Button>
+                            <Radio.Button value="-1">昨天</Radio.Button>
                             <Radio.Button value="7">最近7天</Radio.Button>
                             <Radio.Button value="30">最近30天</Radio.Button>
 
                         </Radio.Group>}>
-                        <ReactHighcharts config = {config}></ReactHighcharts>
+                        <Dropdown overlay={this.menu}>
+                           <a className="ant-dropdown-link">
+                                <Button>请选择指标</Button><Icon type="down" />
+                           </a>
+                         </Dropdown>
+                        {this.state.chart?(<ReactHighcharts config = {config}></ReactHighcharts>):(null)}
 
                     </Card>
                 </div>
@@ -138,8 +163,9 @@ class Trend extends React.Component {
         <Row gutter={16}>
             <Col className="gutter-row" md={24}>
                 <div className="gutter-box">
-                    <Card title="可展开" bordered={false}>
-                    <Table columns={columns} dataSource={data} />
+                    <Card title="" bordered={false}>
+                    {this.state.listData?(<Table pagination={false} columns={columns} dataSource={this.state.listData} />):(null)}
+
                     </Card>
                 </div>
             </Col>
